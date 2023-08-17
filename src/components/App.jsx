acb032e3e15afaf909904b8c61a4295e6ca0e35d
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-08-17 21:04:04 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,103 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar';
+import ImageGallery from "./ImageGallery/ImageGalleryItem/ImageGallery"
+import Button from './Button';
+import SearchApi from "./SearchApi"
+import Loader from './Loader/Loader';
+import Modal from './Modal';
+import css from "./App.module.css";
+
+
+class App extends Component {
+  state = {
+    value: null,
+    page: 1,
+    gallery: [],
+    status: 'allow',
+    showModal: false,
+    largeImage:"",
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { status, value, page } = this.state;
+
+    if (status === 'allow') {
+      this.setState({ status: 'loading' });
+      SearchApi(value, page)
+        .then(response => response.json())
+        .then(ar =>
+          this.setState(
+            ({ gallery }) => ({
+              gallery: [...gallery, ...ar.hits],
+              status: 'deny',
+            }),
+            this.alertEmptyArray(ar.hits.length)
+          )
+        );
+    }
+  }
+
+  alertEmptyArray = (value) => {
+    if (!value) {
+      return alert ("Ooops there is no images, try another search!")
+    }
+  }
+
+  openModal = (img) => {
+    this.setState({ showModal: true });
+    this.setState({largeImage:img})
+  };
+
+  closeModal = () => {
+    this.setState({ showModal:false });
+  }
+
+  onSubmit = name => {
+    if (name === this.state.value) {
+      return alert('Try another input value!');
+    }
+    this.setState({ value: name, page: 1, gallery: [], status: 'allow' });
+  };
+
+  onMore = () => {
+    this.setState(prevSatate => ({
+      page: prevSatate.page + 1,
+      status: 'allow',
+    }));
+  };
+
+  render() {
+    const { gallery, status, showModal } = this.state;
+    return (
+      <section>
+        <div className={css.App}>
+          <Searchbar onChange={this.onSubmit} />
+          <ImageGallery gallery={gallery} openModal={this.openModal} />
+          {gallery.length > 0 && (
+            <div className={css.buttonContainer}>
+              <Button onMore={this.onMore} />
+            </div>
+          )}
+          {status === 'loading' && (
+            <div className={css.loader}>
+              <Loader />
+            </div>
+          )}
+          {showModal && (
+            <Modal onClose={this.closeModal}>
+              <img
+                className={css.img}
+                src={this.state.largeImage}
+                width="900"
+                alt=""
+              />
+            </Modal>
+          )}
+        </div>
+      </section>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from "./Searchbar.module.css";
+
+
+class Searchbar extends Component{
+    state = {
+      value: '',
+    };
+  
+    onSubmit = (e) => {
+      e.preventDefault();
+      if (this.state.value === "") {
+        return alert("Input field is empty!");
+      }
+      this.props.onChange(this.state.value);
+      this.setState({ value: "" });
+    }
+  
+    handleChange = e => {
+      this.setState({value: e.currentTarget.value})
+    };
+
+    render(){
+        return (
+            <header className= {css.searchbar}>
+  <form className={css.SearchForm} onSubmit={this.onSubmit}>
+    <button type="submit" classNameme={css.SearchFormButton}>
+      <span className={css.SearchFormButtonLabel}>Search</span>
+    </button>
+
+    <input
+            onChange={this.handleChange}
+            className={css.SearchFormInput}
+            type="text"
+            value={this.state.value}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
+
+Searchbar.propTypes = {
+  onChange: PropTypes.func.isRequired,
+};
