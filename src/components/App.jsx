7d8cc0707ba1e6296f9f56eed8d7401ce88a0146
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-08-26 21:12:14 +0300

    ./

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import Searchbar from './Searchbar';
-import ImageGallery from "./ImageGallery/ImageGalleryItem/ImageGallery"
+import ImageGallery from "./ImageGallery/ImageGalleryItem/ImageGalleryItem"
 import Button from './Button';
 import SearchApi from "./SearchApi"
 import Loader from './Loader/Loader';
--- src/components/SearchApi/SearchApi.jsx
+++ src/components/SearchApi/SearchApi.jsx
@@ -1,6 +1,6 @@
 
 function SearchApi(value, pageNumber = 1) {
-    const KEY = "5f2a66e63fa9a8139a0b7e8b9aba27ca";
+    const KEY = "36976828-185b29cf4941df1518df1f340";
     return fetch(`https://pixabay.com/api/?q=cat&page=${pageNumber}&key=${KEY}&q=${value}&image_type=photo&orientation=horizontal&per_page=12`);
 }
 
